d47ccd630285ea1c04827c846ccff6a259e69f98
0nepeop1e <engshun.lim@gmail.com>
2025-01-21 14:24:41 +0800

    add favicon

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -1,9 +1,10 @@
 <!DOCTYPE html>
 <html>
 <head>
-    <meta charset="utf-8" />
-    <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0" />
-    <link rel="icon" href="{{ asset('/favicon.ico') }}" type="image/x-icon" />
+    <meta charset="utf-8"/>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0"/>
+    <link rel="icon" href="{{ asset('/favicon.svg') }}" type="image/x-icon"/>
+    <title>{{ config('app.name') }}</title>
     @vite('resources/css/app.css')
     @vite('resources/js/app.ts')
     @inertiaHead
