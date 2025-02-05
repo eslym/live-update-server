6f273d219500b6f985cb184f137c955a9df7227f
0nepeop1e <engshun.lim@gmail.com>
2025-02-06 05:22:30 +0800

    use css for syntax highlight dark theme instead of rerender

--- resources/js/components/CodeSnippet.svelte
+++ resources/js/components/CodeSnippet.svelte
@@ -1,7 +1,8 @@
 <script lang="ts">
+    import type { Root, RootContent } from 'hast';
     import { shiki } from '@/lib/shiki';
-    import { dark } from '@/lib/config';
     import { Copy01Icon } from '@eslym/hugeicons-svelte';
+    import parseInline from 'inline-style-parser';
 
     let { endpoint }: { endpoint: string } = $props();
 
@@ -44,21 +45,72 @@ if (Capacitor.isNativePlatform()) {
         });
 }
 `);
-    let theme = $derived(localStorage.getItem('theme'));
     let highlighted = $derived(
-        shiki
-            .codeToHtml(code, {
-                lang: 'typescript',
-                theme: `github-${theme ?? (dark.current ? 'dark' : 'light')}`
-            })
-            .replace('<pre class="', '<pre class="h-0 min-h-0 flex-grow overflow-auto ')
+        shiki.codeToHast(code, {
+            lang: 'typescript',
+            themes: {
+                light: 'github-light',
+                dark: 'github-dark'
+            }
+        })
     );
+
+    const classes = {
+        pre: 'h-0 min-h-0 flex-grow overflow-auto'
+    } as Record<string, string>;
+
+    const styleMap = {
+        color: '--shiki-light',
+        'background-color': '--shiki-light-bg',
+        'font-style': '--shiki-light-font-style',
+        'font-weight': '--shiki-light-font-weight',
+        'text-decoration': '--shiki-light-text-decoration'
+    } as Record<string, string>;
+
+    function mergeProp(props: Record<string, any>, classes: string) {
+        if (props.class) {
+            props.class += ` ${classes}`;
+        } else {
+            props.class = classes;
+        }
+        if (props.style) {
+            const parsed = parseInline(props.style);
+            let transformed = '';
+            for (const token of parsed) {
+                if (token.type === 'declaration') {
+                    token.property = styleMap[token.property] || token.property;
+                    transformed += `${token.property}: ${token.value};`;
+                }
+            }
+            props.style = transformed;
+        }
+        return props;
+    }
 </script>
+
+{#snippet render_hast(token: RootContent | Root)}
+    {#if token.type === 'element'}
+        <svelte:element
+            this={token.tagName}
+            {...mergeProp(token.properties, classes[token.tagName])}
+        >
+            {#each token.children as child}
+                {@render render_hast(child)}
+            {/each}
+        </svelte:element>
+    {:else if token.type === 'text'}
+        {token.value}
+    {:else if token.type === 'root'}
+        {#each token.children as child}
+            {@render render_hast(child)}
+        {/each}
+    {/if}
+{/snippet}
 
 <div
     class="textarea max-w-5xl font-mono h-[600px] overflow-hidden w-full p-0 flex flex-col relative"
 >
-    {@html highlighted}
+    {@render render_hast(highlighted)}
     <button
         type="button"
         class="btn btn-xs btn-secondary absolute right-6 top-2 pr-3"
